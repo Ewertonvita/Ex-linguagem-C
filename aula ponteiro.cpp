d7fa733277fcf8d75ae9ff67216a14178f403c9e
Ewerton MSD Jesus <108229218+Ewertonvita@users.noreply.github.com>
2023-11-09 09:17:10 -0300

    Add files via upload

---
+++ aula ponteiro.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main (void) 
+{
+	int  x,y,*ponteiro;
+	x=27;
+	y=150;
+	
+	printf("%i",&x);// o endereço de memoria de x 6487576
+	
+	getchar ();
+	return 0 ; 
+	
+	
+	
+	}
---
+++ contador até 100.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+// contador em c //
+int main() 
+        { 
+        int cont=0;
+        for (cont = 1; cont<=100;cont++){
+        printf (" * %d  ",   cont);
+        }
+    return 0;
+        }
